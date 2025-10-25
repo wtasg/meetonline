e805ba00d1b1dc61ea42cd35b16b480751bb2287
wtasg <225132696+wtasg@users.noreply.github.com>
2025-10-25 06:32:34 +0000

    feature/database: Working database through commandline and script

--- database/run-meetonline.sh
+++ database/run-meetonline.sh
@@ -12,15 +12,14 @@ podman volume create pgdata
 dos2unix entrypoint.sh
 chmod u+x entrypoint.sh
 
-podman build --no-cache -t meetonline .
+podman build --no-cache --tag meetonline .
 
 podman run \
   --name meetonline \
   --env-file local.env \
-  -p 54321:5432 \
-  -v pgdata:/var/lib/postgresql/data \
+  --publish 54321:5432 \
+  --volume pgdata:/var/lib/postgresql/data \
   --detach localhost/meetonline:latest
 
 echo "MeetOnline database container is running on port 54321"
-podman ps --filter "name=meetonline"
-echo "To connect: psql -h localhost -p 54321 -U <username> -d <database>"
+echo "To connect: podman exec --interactive --tty meetonline psql --host=localhost --port=5432 --dbname=meetonline --username myuser --password"
