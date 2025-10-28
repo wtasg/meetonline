070c22c133c4d8ec4afaf637920b07e786204da8
wtasg <225132696+wtasg@users.noreply.github.com>
2025-10-28 04:55:25 +0000

    fe: Fix lint and other issues

--- client/react-client-app/src/App.jsx
+++ client/react-client-app/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
         <>
             <Top />
         </>
-    )
+    );
 }
 
 export default App;
--- client/react-client-app/vite.config.js
+++ client/react-client-app/vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-    coverage: {
-      reporter: ['text', 'json', 'html'],
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.js',
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+        },
     },
-  },
 });
