76e73fb9ef6ffa8c14b9fa52e618024e8beb95f9
praakhartripathi <gurawliprakhar@gmail.com>
2025-11-05 07:13:54 +0000

    be: use morgan to log http request | closes #102

--- server/node-server-app/src/server.js
+++ server/node-server-app/src/server.js
@@ -2,6 +2,7 @@ import { existsSync, mkdirSync } from "node:fs";
 import { resolve as pathResolve } from "node:path";
 
 import express from "express";
+import morgan from "morgan";
 import * as cookieParserPkg from "cookie-parser";
 
 import { SERVER_PORT } from "./config.js";
@@ -11,14 +12,24 @@ import { setupAuthHandlers } from "./handlers/authHandler.js";
 import { setupGracefulShutdown } from "./utils/gracefulSetup.js";
 import { setupUploadHandler } from "./handlers/uploadHandler.js";
 import { setupDirectories } from "./utils/fs.js";
+import { loadEnv } from "./utils/env.js";
 
 const app = express();
 const cookieParser = cookieParserPkg.default;
+
+loadEnv(process.env.NODE_ENV);
+
+const isProduction = process.env.NODE_ENV === "production";
 
 setupDirectories({ exists: existsSync, mkdir: mkdirSync });
 /* Middlewares */
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+    morgan(isProduction ? "combined" : "dev")
+);
+
 setupCorsMiddleware(app);
 
 app.use("/uploads", express.static(pathResolve(process.cwd(), "server/node-server-app/uploads")));
