1008686f13ecf4bdcc83f79ef63b6c8a77a03e5e
praakhartripathi <gurawliprakhar@gmail.com>
2025-11-10 12:27:39 +0530

    working on ract client handle https backend request

--- client/react-client-app/vite.config.js
+++ client/react-client-app/vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "node:fs";
+
+const key = fs.readFileSync("./certs/server.key");
+const cert = fs.readFileSync("./certs/server.crt");
 
 export default defineConfig({
     plugins: [react()],
+    server: {
+        host: true,
+        https: { key, cert },
+        port: 5173,
+        proxy: {
+            "/api": {
+                target: "https://localhost:9443",
+                changeOrigin: true,
+                secure: false,
+            }
+        }
+    },
     test: {
         globals: true,
         environment: "jsdom",
