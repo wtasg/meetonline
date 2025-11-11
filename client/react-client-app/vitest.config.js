35091adb9afe5a642635830c8594704897eb3658
wtasg <225132696+wtasg@users.noreply.github.com>
2025-11-11 14:48:51 +0530

    vitest: ues exclude instead of nonstandard dir

--- client/react-client-app/vitest.config.js
+++ client/react-client-app/vitest.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     test: {
-        dir: "src",
+        exclude: ["**/node_modules/**", "**/dist/**", "**/tests/**"],
         environment: "jsdom",
         globals: true,
         setupFiles: "./src/setupTests.js",
