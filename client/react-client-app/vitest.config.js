615d3a31d9a8850d9b30179f45ab9c96ac01580e
wtasg <225132696+wtasg@users.noreply.github.com>
2025-11-11 14:17:48 +0530

    fix: update vitest config to only look for tests in src dir

--- client/react-client-app/vitest.config.js
+++ client/react-client-app/vitest.config.js
@@ -2,19 +2,20 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setupTests.js",
-    environmentOptions: {
-      jsdom: {
-        resources: "usable",
-      },
+    plugins: [react()],
+    test: {
+        dir: "src",
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./src/setupTests.js",
+        environmentOptions: {
+            jsdom: {
+                resources: "usable",
+            },
+        },
     },
-  },
-  esbuild: {
-    jsx: "automatic",
-    jsxImportSource: "react",
-  },
+    esbuild: {
+        jsx: "automatic",
+        jsxImportSource: "react",
+    },
 });
