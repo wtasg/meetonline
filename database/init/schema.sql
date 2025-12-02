6b030ccb9e1d24dbb4b79ab228eb731770922ba7
praakhartripathi <gurawliprakhar@gmail.com>
2025-12-02 18:10:06 +0530

    database: implement event table

--- database/init/schema.sql
+++ database/init/schema.sql
@@ -67,7 +67,7 @@ create table if not exists user_profile
 
 comment on column user_profile.id is 'user profile id';
 
-comment on column user_profile.user_id is 'user account it';
+comment on column user_profile.user_id is 'user account id';
 
 comment on column user_profile.profile_name is 'user''s profile''s name';
 
@@ -86,3 +86,97 @@ alter table user_profile
 
 -- USER PROFILE : END
 
+-- EVENT : START
+create table if not exists event
+(
+    id                         bigserial,
+
+    -- Orgnisers
+    organiser_id               bigint                              not null,
+    organisers                  text,
+
+    -- Event fields
+    title                      varchar(1024)                       not null,
+    description                text,
+    online_location            varchar(1024),
+
+    -- Timing
+    start_at                   timestamp                           not null,
+    end_at                     timestamp                           not null,
+
+    -- Payment
+    is_paid                    boolean   default false             not null,
+
+    -- Broadcast
+    is_broadcast               boolean   default false             not null,
+    broadcast_type             varchar(64),
+
+    -- Tags and Categories
+    tags                       text,
+    categories                 text,
+
+    -- Visibility and Interaction
+    is_interactive             boolean   default true              not null,
+    is_anonymous               boolean   default false             not null,
+
+    -- Engagement
+    interested                 text,
+
+    -- Attachments
+    attached_documents         text,
+
+    -- other
+    group_id                   bigint default null,
+    created_at                 timestamp default    CURRENT_TIMESTAMP   not null,
+    modified_at                 timestamp default   CURRENT_TIMESTAMP   not null,
+    is_deleted                 boolean   default    false               not null,
+    is_hidden                  boolean   default    false               not null,
+    is_archived                boolean   default    false               not null
+);
+
+comment on table event is 'Events created by user profiles';
+comment on column event.id is 'Primary key for events table';
+comment on column event.organiser_id is 'FK: profile_id who created the event';
+comment on column event.organisers is 'FK: profile who created the event';
+comment on column event.title is 'Title of the event or meeting';
+comment on column event.description is 'Description of the event';
+comment on column event.online_location is 'Meeting URL or Meeting ID';
+comment on column event.start_at is 'Event start timestamp';
+comment on column event.end_at is 'event end time';
+comment on column event.is_paid is 'Whether the event requires payment';
+comment on column event.is_broadcast is 'Whether the event is a broadcast (YT Live, Twitch, prerecorded)';
+comment on column event.broadcast_type is 'Type of broadcast: youtube, twitch, prerecorded';
+comment on column event.tags is 'hashtag for event';
+comment on column event.categories is 'categories for event';
+comment on column event.is_interactive is 'Will organiser interact with attendees?';
+comment on column event.is_anonymous is 'Whether attendees can join anonymously';
+comment on column event.interested is 'interested users';
+comment on column event.attached_documents is 'attach document link';
+comment on column event.group_id is 'Reserved for groups feature';
+comment on column event.created_at is 'Record creation timestamp';
+comment on column event.modified_at is 'Record update timestamp';
+comment on column event.is_deleted is 'delete flag';
+comment on column event.is_hidden is 'whether event is hidden from public listing';
+comment on column event.is_archived is 'whether event is archived';
+
+alter table event
+    owner to myuser;
+
+-- Primary Key
+alter table event
+    add constraint event_pk
+        primary key (id);
+
+-- Foreign Keys
+alter table event
+    add constraint event_organiser_id_fk
+        foreign key (organiser_id) references user_profile (id);
+
+-- Indexes
+create index if not exists event_start_at_index
+    on event (start_at);
+
+create index if not exists event_organiser_id_index
+    on event (organiser_id);
+
+-- EVENT : END
